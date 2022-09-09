0ddb817d3f54ddf42b3711fbaa4fe3cadb1c6e41
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-09 12:57:45 +0100

    Update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,36 @@
-#include <stdio.h>
-/**
- * main - program that prints numbers from 0 to 99.
- * Return: 0
+/*
+ * File: 100-print_comb3.c
+ * Auth: muhammed abdulrahaman
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int c = 0;
+	int digit1, digit2;
 
-	while (c <= 99)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		putchar(c / 10 + '0');
-		putchar(c % 10 + '0');
-		if (c != 99)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
-		c++;
 	}
-putchar('\n');
-return (0);
+
+	putchar('\n');
+
+	return (0);
 }
