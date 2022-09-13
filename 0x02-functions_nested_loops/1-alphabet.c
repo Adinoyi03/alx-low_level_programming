0df754f0e645aa8e4efb2a48b23336b47ecb043c
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-13 13:54:05 +0100

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,17 @@
  *File: 1-alphabet.c
  *Auth: Muhammed Abdulrahaman Adinoyi
  */
- #include "main.h"
- /**
-  *print_alphabets-prints the alphabets in lower case, followed by a new line
-  */
- void print_alphabet(void)
- {
-     char letter;
-     
-     for (letter = 'a'; letter <= 'z'; letter++)
-     {
-         _putchar(letter);
-     }
-     _putchar("\n");
- }
+#include "main.h"
+/**
+ *print_alphabets-prints the alphabets in lower case, followed by a new line
+ */
+void print_alphabet(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        _putchar(letter);
+    }
+    _putchar("\n");
+}
