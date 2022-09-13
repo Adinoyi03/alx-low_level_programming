24fe0644a3a039e16835c4a25a9db590c5f9bb76
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-13 04:36:44 +0100

    Update

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Muhammed Abdulrahaman adinoyi
+ *
+ */
+#include "holberton.h"
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
