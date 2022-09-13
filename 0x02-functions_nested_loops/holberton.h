9ee98178c845ffb6db45867a5fa75d4907648a66
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-13 09:54:45 +0100

    Update main.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
- * File: holberton.h
+ * File: main.h
  * Auth: Muhammed Abdulrahaman Adinoyi
  * Desc: Header file containing declarations for all functions
  * used in the 0x02-functions_nested_loops directory.
