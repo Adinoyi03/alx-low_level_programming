9354299b55ff2df5c91aa3d68d5b213f713fe118
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-13 12:39:59 +0100

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-	_putchar('_');
+        _putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('t');
+        _putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
