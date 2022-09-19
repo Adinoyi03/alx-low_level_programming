6bdac513d0e9a82b0591b702739fe11a32b34b3f
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-19 07:14:38 +0100

    Create

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,23 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
 #include "main.h"
+
 /**
  * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ * @s: The string to be printed.
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int len = 0, index;
 
-	while (s[i++])
+	while (s[index++])
 		len++;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
 	_putchar('\n');
 }
