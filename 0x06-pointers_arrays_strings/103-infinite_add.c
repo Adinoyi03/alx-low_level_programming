e979390b4a3e86b2257a474395d630fa6c0c05d4
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-09-22 09:56:47 +0100

    Updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,9 +1,4 @@
-/*
- * File: 103-infinite_add.c
- * Auth: Muhammed Abdulrahaman Adinoyi
- */
-
-#include "holberton.h"
+#include "main.h"
 
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
