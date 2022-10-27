c1b06da25820e89582b27ec56f90f2394da88840
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2022-10-27 15:46:15 +0100

    Update

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,51 +1,29 @@
 #include "main.h"
 
 /**
- * _strlen - returns len of a string
- * @str: string
- * Return: length of the string
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
-
-unsigned int _strlen(const char *str)
-{
-	unsigned int len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: string with 0 and 1 to convert
- * Return: converted number or 0 if there is no 0 or 1 to convert
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x2 = 1, conv_val = 0, len;
 	int i;
+	unsigned int num;
 
-	if (b == NULL)
-	{
+	num = 0;
+	if (!b)
 		return (0);
-	}
-
-	len = _strlen(b);
-
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-		{
 			return (0);
-		}
-		if (b[i] == '1')
-		{
-			conv_val += x2;
-		}
-		x2 *= 2;
 	}
-	return (conv_val);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
